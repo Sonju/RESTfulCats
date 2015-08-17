51cef2b2bbe9ec8dec8ab00426938e80fdfa0723
Sonju Walker <sonjuwalker@gmail.com>
2015-08-17 14:56:48 -0500

    udating gemfile

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https:rubygems.org'
 
 gem 'Sinatra'
+gem 'pg'
 gem 'Sinatra-activerecord'
 gem 'json'
---
+++ app.rb
@@ -0,0 +1,8 @@
+require 'bundler'
+Bundler.require
+
+ActiveRecord::Base.establish_connection(
+  :adapter => 'postgresql'
+  :database => 'restfulcats'
+
+)
